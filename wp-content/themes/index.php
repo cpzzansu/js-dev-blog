9f1b7cbbdd096b7d1c20a60b2cdb9316aff2bd0e
cpzzansu <cpzzansu@gmail.com>
2023-12-18 06:07:04 +0000

    수정

--- wp-content/themes/index.php
+++ wp-content/themes/index.php
@@ -1,5 +1,1 @@
-<?php get_header(); ?>
-    <div class="container">
-        <h1 class="display-1">Hello, world!</h1>
-    </div>
-<?php get_footer(); ?>
+<?php
---
+++ wp-content/themes/js-dev-blog/index.php
@@ -0,0 +1,5 @@
+<?php get_header(); ?>
+    <div class="container">
+        <h1 class="display-1">Hello, world!</h1>
+    </div>
+<?php get_footer(); ?>
