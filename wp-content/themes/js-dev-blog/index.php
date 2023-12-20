25467051639e74b51a0a2cdd3f1e4ddb7b2170d1
junsu <cpzzansu@gmail.com>
2023-12-20 14:14:22 +0900

    css파일 링크

--- wp-content/themes/js-dev-blog/index.php
+++ wp-content/themes/js-dev-blog/index.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
     <div class="container">
+        <div style="height: 300px; background-color: red">
+
+        </div>
         <h1 class="display-1">Hello, world!</h1>
     </div>
 <?php get_footer(); ?>
