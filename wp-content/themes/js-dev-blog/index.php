e5005ad4beb85639ae587e62e3599517818250fe
junsu <cpzzansu@gmail.com>
2023-12-20 15:07:37 +0900

    css파일 링크

--- wp-content/themes/js-dev-blog/index.php
+++ wp-content/themes/js-dev-blog/index.php
@@ -1,9 +1,11 @@
 <?php get_header(); ?>
-    <div class="container">
-        <div style="height: 300px;">
+<div class="container">
+    <div style="height: 300px; align-content: center">
+        <div>
             <div style="font-family: 'GmarketSansMedium'; font-size: 40px; color: #383838;">Story of</div>
             <div style="font-family: 'GmarketSansBold'; font-size: 40px; color: #383838;">Developer JS</div>
         </div>
-        <h1 class="display-1" style="font-family: 'GmarketSansBold'">Hello, world!</h1>
     </div>
+    <h1 class="display-1" style="font-family: 'GmarketSansBold'">Hello, world!</h1>
+</div>
 <?php get_footer(); ?>
