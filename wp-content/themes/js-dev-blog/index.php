a02318272bcf3320120d8acc8b0a91006fff18df
junsu <cpzzansu@gmail.com>
2023-12-20 15:03:52 +0900

    css파일 링크

--- wp-content/themes/js-dev-blog/index.php
+++ wp-content/themes/js-dev-blog/index.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
     <div class="container">
         <div style="height: 300px;">
-            <div style="font-family: 'GmarketSansBold'; font-size: 36px">Developer JS</div>
-            <div>성장기</div>
+            <div style="font-family: 'GmarketSansBold'; font-size: 40px; color: #383838;">Developer JS</div>
+            <div style="font-family: 'GmarketSansMedium'; font-size: 40px; color: #383838;">성장기</div>
         </div>
         <h1 class="display-1" style="font-family: 'GmarketSansBold'">Hello, world!</h1>
     </div>
