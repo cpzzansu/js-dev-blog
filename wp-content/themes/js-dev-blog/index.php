ab9110351facf94bb17aab1d1269c8b6616b6337
junsu <cpzzansu@gmail.com>
2023-12-26 11:46:05 +0900

    css파일 링크 확인

--- wp-content/themes/js-dev-blog/index.php
+++ wp-content/themes/js-dev-blog/index.php
@@ -3,7 +3,7 @@
     <div class="row" style="height: 300px;">
         <div class="d-flex align-items-center justify-content-center">
             <div>
-                <div class="red-color" style="font-family: 'GmarketSansMedium'; font-size: 40px; color: #383838;">Story of</div>
+                <div class="red-color" style="font-family: 'GmarketSansMedium'; font-size: 40px;">Story of</div>
                 <div style="font-family: 'GmarketSansBold'; font-size: 40px; color: #383838; margin-left: 90px">Developer JS</div>
             </div>
         </div>
