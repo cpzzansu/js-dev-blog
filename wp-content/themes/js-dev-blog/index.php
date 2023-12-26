d3bae8c131a146328360813e574b264cadfa4064
junsu <cpzzansu@gmail.com>
2023-12-26 17:13:20 +0900

    블로그 리스트 만들기

--- wp-content/themes/js-dev-blog/index.php
+++ wp-content/themes/js-dev-blog/index.php
@@ -4,9 +4,22 @@
         <div class="d-flex align-items-center justify-content-center">
             <div>
                 <div style="font-family: 'GmarketSansMedium'; font-size: 40px; color: #383838;">Story of</div>
-                <div style="font-family: 'GmarketSansBold'; font-size: 40px; color: #383838; margin-left: 90px">Developer JS</div>
+                <div style="font-family: 'GmarketSansBold'; font-size: 40px; color: #383838; margin-left: 90px">
+                    Developer JS
+                </div>
             </div>
         </div>
     </div>
+    <?php
+        if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+
+            <h1>hi</h1>
+
+            <?php endwhile;
+        else: _e('Sorry, no posts found.');
+        endif;
+    ?>
+
 </div>
 <?php get_footer(); ?>
