0315e573b104071dbe5fb2a141b4e147be358f37
junsu <cpzzansu@gmail.com>
2023-12-28 15:14:08 +0900

    목록 스타일 변경

--- wp-content/themes/js-dev-blog/index.php
+++ wp-content/themes/js-dev-blog/index.php
@@ -27,7 +27,7 @@
                 <p class="substr"><?php echo mb_substr( strip_tags( get_the_content() ), 0, 100 ) ?></p>
             </a>
 
-            <div class="d-flex row">
+            <div class="row">
                 <!-- 블로그 글 리스트 카테고리 -->
                 <?php the_category( ', ' ); ?>
 
